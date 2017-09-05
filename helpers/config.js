868dd110a581882a76f29ee2ae2c1a2afe0a35e2
pcboby <pcboby@126.com>
2017-09-05 16:56:20 +0800

    update

--- helpers/config.js
+++ helpers/config.js
@@ -1,4 +1,18 @@
 define(function() {
+
+    //请求之前执行
+    webix.attachEvent("onBeforeAjax", function(mode, url, data, request, headers, files, promise) {
+        headers["Content-type"] = "application/json";
+    });
+    //请求出错时执行
+    webix.attachEvent('onAjaxError', function(request) {});
+    //错误数据时执行
+    webix.attachEvent('onLoadError', function(text, xml, xhttp, obj) {});
+
+    webix.ajax.$callback = function(owner, call, text, data, x, isError) {
+        console.log('isCallBack')
+    };
+
     var config = {
         id: 'SDCloud',
         name: 'SDCloud Admin',
